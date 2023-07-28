6f318c4c8451cd9b1ea90ad8c8c1fc6ad7eaf264
Shivendra Singh <sssengar362@gmail.com>
2023-07-29 02:22:48 +0530

    React_Nodejs

---
+++ Front_React/src/components/Formtable.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../App.css";
+import { MdClose } from "react-icons/md";
+
+const Formtable = ({ handleSubmit, handleOnChange, handleclose, rest }) => {
+  return (
+    <div className="addContainer">
+      <form onSubmit={handleSubmit}>
+        <div className="close-btn" onClick={handleclose}>
+          <MdClose />
+        </div>
+        <label htmlFor="name">Name : </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleOnChange}
+          value={rest.name}
+          placeholder="Enter your Name"
+        />
+
+        <label htmlFor="email">Email : </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={handleOnChange}
+          value={rest.email}
+          placeholder="Enter Email ID"
+        />
+
+        <label htmlFor="mobile">Mobile : </label>
+        <input
+          type="number"
+          id="mobile"
+          name="mobile"
+          onChange={handleOnChange}
+          value={rest.mobile}
+          placeholder="Enter Mobile No"
+        />
+
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Formtable;
